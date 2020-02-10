163dfa6a271ecc33ab9d99a13b2f9999554e5ce3
daiju81 <1d07c.m8k.prv@gmail.com>
2020-02-10 21:35:09 +0700

    design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,9 +19,15 @@
     <p>Treapは仲間との思い出を</p>
     <p>デジタルフォトアルバムとして</p>
     <p>記憶に残すサービス</p>
+@auth
+
+<a class="" href="/">ユーザーページ</a>
+
+@endauth
   </div>
 
 
+@guest
 
   <div class="text-center">
     <button type="submit" class="mx-auto submit text-center btn btn-primary">
@@ -29,7 +35,7 @@
     </button>
   </div>
 </div>
-
+@endguest
 
 
 @endsection
